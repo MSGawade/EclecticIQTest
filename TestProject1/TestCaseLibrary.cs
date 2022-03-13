c21c8b8304beadad923b3ea633fd0f19651bcafd
Mahadev Gawade <Mahadev.Gawade@emc.com>
2022-03-14 00:47:47 +0530

    ...

--- TestProject1/TestCaseLibrary.cs
+++ TestProject1/TestCaseLibrary.cs
@@ -3,8 +3,11 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Collections;
 
 namespace TestProject1
 {
@@ -48,6 +51,16 @@ namespace TestProject1
 
         IList<IWebElement> noOfRows => driver.FindElements(By.XPath("//div[@class='table-row']"));
 
+        //get the row count for impact score
+        IList<IWebElement> impactScoreRows => driver.FindElements(By.XPath("//div[@class='table-data data-averageImpact']"));
+
+        //get the row count for number of cases
+        IList<IWebElement> noOfCasesRows => driver.FindElements(By.XPath("//div[@class='table-data data-cases']"));
+
+        //get the row count for name column
+        IList<IWebElement> noOfNameRows => driver.FindElements(By.XPath("//div[@class='table-data data-name']"));
+
+        //below scripts TestMethod1 to TestMethod4 validates the table data dynamically based on user input
         [TestMethod]
         [Description("Combination filter data by names and sort data ='Name'")]
         public void TestMethod1()
@@ -286,6 +299,140 @@ namespace TestProject1
                 {
                     Assert.IsTrue(false, "No rows found with the complexity value");
                 }
+
+                Thread.Sleep(2000);
+            }
+            catch (Exception e)
+            {
+                //failure stack trace
+                Assert.Fail(e.Message);
+            }
+
+        }
+
+        //below scripts TestMethod5 to TestMethod7 validate the data if it's appearing in ascending order or not
+        [TestMethod]
+        [Description("Verify ascending order when filter data by complexity ='low' and sort data ='Impact score'")]
+        public void TestMethod5()
+        {
+            string complexity = "low";
+            string sortDataValue = "Impact score";
+            try
+            {
+                //enter filter data
+                filterData.Clear();
+                filterData.SendKeys(complexity);
+                Thread.Sleep(1000);
+
+                //select sort data as Name
+                sortData.SelectByText(sortDataValue);
+                
+                IList<float> obtainedList = new List<float>();
+                
+                //add values to list
+                foreach (IWebElement we in impactScoreRows)
+                {
+                    float i = (float)(Double.Parse(we.Text.ToString()));
+
+                    obtainedList.Add(i);
+                }
+
+                //Validate whether values are displayed in ascending order for impact score column
+                Assert.IsTrue(obtainedList.OrderBy(c => c).SequenceEqual(obtainedList));
+
+                Thread.Sleep(2000);
+            }
+            catch (Exception e)
+            {
+                //failure stack trace
+                Assert.Fail(e.Message);
+            }
+
+        }
+
+        [TestMethod]
+        [Description("Verify ascending order when filter data is 'a' and sort data ='number of cases'")]
+        public void TestMethod6()
+        {
+            string filterData1 = "a";
+            string sortDataValue = "Number of cases";
+            try
+            {
+                //enter filter data
+                filterData.Clear();
+                filterData.SendKeys(filterData1);
+                Thread.Sleep(1000);
+
+                //select sort data as Name
+                sortData.SelectByText(sortDataValue);
+
+                IList<int> obtainedList = new List<int>();
+                
+                foreach (IWebElement we in noOfCasesRows)
+                {
+                    //replace k=1000,M=100000,B=10000000
+
+                    string value = we.Text;
+                    int i = 0;
+                    if (!value.Contains("k") && !value.Contains("M") && !value.Contains("B"))
+                    {
+                        i = int.Parse(value);
+                    }
+                    else if (value.Contains("k"))
+                    {
+                        i = (int)(Double.Parse(value.Substring(0, value.Length - 1)) * 1000);
+                    }
+                    else if (value.Contains("M"))
+                    {
+                        i = (int)(Double.Parse(value.Substring(0, value.Length - 1)) * 100000);
+                    }
+                    else if (value.Contains("B"))
+                    {
+                        i = (int)(Double.Parse(value.Substring(0, value.Length - 1)) * 10000000);
+                    }
+
+                    obtainedList.Add(i);
+                }
+
+                //validate ILIST<int> is in sequence or not
+                Assert.IsTrue(obtainedList.OrderBy(c => c).SequenceEqual(obtainedList));
+
+                Thread.Sleep(2000);
+            }
+            catch (Exception e)
+            {
+                //failure stack trace
+                Assert.Fail(e.Message);
+            }
+
+        }
+
+        [TestMethod]
+        [Description("Verify ascending order when filter data is not entered and sort data ='Name'")]
+        public void TestMethod7()
+        {
+            string filterData1 = "";
+            string sortDataValue = "Name";
+            try
+            {
+                //enter filter data
+                //filterData.Clear();
+                //filterData.SendKeys(filterData1);
+                //Thread.Sleep(1000);
+
+                //select sort data as Name
+                sortData.SelectByText(sortDataValue);
+
+                IList<string> obtainedList = new List<string>();
+
+                //add values to list
+                foreach (IWebElement we in noOfNameRows)
+                {
+                    obtainedList.Add(we.Text);
+                }
+
+                //validate ILIST<int> is in sequence or not
+                Assert.IsTrue(obtainedList.OrderBy(c => c).SequenceEqual(obtainedList));
 
                 Thread.Sleep(2000);
             }
